abaa81a4d3f6486494e792aba66571a5c2ea44b2
Alex Dickerson <aldicke@microsoft.com>
2024-04-19 15:36:08 -0400

    Add project files.

---
+++ DMCockpit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DMCockpit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ DMCockpit/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace DMCockpit
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
